8b59bbe2fc914532eae3145ec33ccb28acbca44c
stephaniejwright <stephanie.j.denis@gmail.com>
2019-04-04 16:56:45 -0700

    biketwn data end of day 4/4/19

--- code/base-R-plot.R
+++ code/base-R-plot.R
@@ -11,6 +11,9 @@ summary(biketown)
 #this is tidyverse
 biketown$hour <- hms(biketown$StartTime) %>% hour()
 
+#assign labels to months
+biketown$month <- mdy(biketown$StartDate) %>% month(label = T, abbr = T)
+
 #this does same result
 stime <- hms(biketown$StartTime)
 biketown$hour <- hour(stime)
@@ -28,3 +31,16 @@ AM_peak <- subset(biketown, hour>=7 & hour<10)
 
 barplot(table(AM_peak$hour))
 
+barplot(biketown$PaymentPlan)
+
+freq_by_month <- table(biketown$month)
+barplot(freq_by_month)
+
+#station analysis
+freq_by_station <- table(biketown$StartHub)
+
+# top 25 stations
+top_25_stations <- sort(freq_by_station, decreasing = T) [1:25]
+dotchart(top_25_stations)
+
+rm (S)
